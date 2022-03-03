1374e5b811095c726937e81af23913b95e5c040a
etke <etk3@protonmail.com>
2022-03-02 18:37:03 -0600

    Gate optional deps behind binja feature

--- build.rs
+++ build.rs
@@ -1,18 +1,23 @@
+#[cfg(feature = "binja")]
 use std::env;
+#[cfg(feature = "binja")]
 use std::fs::File;
+#[cfg(feature = "binja")]
 use std::io::BufReader;
+#[cfg(feature = "binja")]
 use std::path::PathBuf;
 
-#[cfg(target_os = "macos")]
+#[cfg(all(target_os = "macos", feature = "binja"))]
 static LASTRUN_PATH: (&str, &str) =
     ("HOME", "Library/Application Support/Binary Ninja/lastrun");
 
-#[cfg(target_os = "linux")]
+#[cfg(all(target_os = "linux", feature = "binja"))]
 static LASTRUN_PATH: (&str, &str) = ("HOME", ".binaryninja/lastrun");
 
-#[cfg(windows)]
+#[cfg(all(windows, feature = "binja"))]
 static LASTRUN_PATH: (&str, &str) = ("APPDATA", "Binary Ninja\\lastrun");
 
+#[cfg(feature = "binja")]
 // Check last run location for path to BinaryNinja; Otherwise check the default install locations
 fn link_path() -> PathBuf {
     use std::io::prelude::*;
@@ -29,40 +34,42 @@ fn link_path() -> PathBuf {
             Ok(PathBuf::from(binja_path.trim()))
         })
         .unwrap_or_else(|_| {
-            #[cfg(target_os = "macos")]
+            #[cfg(all(target_os = "macos", feature = "binja"))]
             return PathBuf::from(
                 "/Applications/Binary Ninja.app/Contents/MacOS",
             );
 
-            #[cfg(target_os = "linux")]
+            #[cfg(all(target_os = "linux", feature = "binja"))]
             return home.join("binaryninja");
 
-            #[cfg(windows)]
+            #[cfg(all(windows, feature = "binja"))]
             return PathBuf::from(env::var("PROGRAMFILES").unwrap())
                 .join("Vector35\\BinaryNinja\\");
         })
 }
 
 fn main() {
+    #[cfg(feature = "binja")]
+    // do nothing if not building with "binja" feature
     // Use BINARYNINJADIR first for custom BN builds/configurations (BN devs/build server), fallback on defaults
     let install_path =
         env::var("BINARYNINJADIR").map_or_else(|_| link_path(), PathBuf::from);
 
-    #[cfg(target_os = "linux")]
+    #[cfg(all(target_os = "linux", feature = "binja"))]
     println!(
         "cargo:rustc-link-arg=-Wl,-rpath,{},-L{},-l:libbinaryninjacore.so.1",
         install_path.to_str().unwrap(),
         install_path.to_str().unwrap(),
     );
 
-    #[cfg(target_os = "macos")]
+    #[cfg(all(target_os = "macos", feature = "binja"))]
     println!(
         "cargo:rustc-link-arg=-Wl,-rpath,{},-L{},-lbinaryninjacore",
         install_path.to_str().unwrap(),
         install_path.to_str().unwrap(),
     );
 
-    #[cfg(target_os = "windows")]
+    #[cfg(all(target_os = "windows", feature = "binja"))]
     {
         println!("cargo:rustc-link-lib=binaryninjacore");
         println!("cargo:rustc-link-search={}", install_path.to_str().unwrap());
